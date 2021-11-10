ab782f4cd7762d35ba7488ce37ef596f9532fc5a
github <github>
2021-11-10 11:13:57 +0900

    データベースの構造を変更

--- attendance.sql
+++ attendance.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- ホスト: 127.0.0.1
--- 生成日時: 2021-10-02 06:05:02
--- サーバのバージョン： 10.4.20-MariaDB
--- PHP のバージョン: 8.0.9
+-- 生成日時: 2021-11-10 03:11:14
+-- サーバのバージョン： 10.4.21-MariaDB
+-- PHP のバージョン: 8.0.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -30,10 +30,41 @@ SET time_zone = "+00:00";
 CREATE TABLE `attendance_log` (
   `id` int(11) NOT NULL,
   `students_id` int(11) NOT NULL,
+  `students_classes_id` varchar(16) NOT NULL,
   `day_of_week` varchar(12) NOT NULL,
   `timetable_times_id` int(2) DEFAULT NULL,
   `timetable_subjects_id` varchar(40) DEFAULT NULL,
   `attendance_status` varchar(12) NOT NULL,
+  `update_time` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- テーブルのデータのダンプ `attendance_log`
+--
+
+INSERT INTO `attendance_log` (`id`, `students_id`, `students_classes_id`, `day_of_week`, `timetable_times_id`, `timetable_subjects_id`, `attendance_status`, `update_time`) VALUES
+(1, 200001, 'ig21', '月曜日', 3, 'ゲームプログラミングc', '出席', '2021-10-25 10:47:15'),
+(2, 200001, 'ig21', '火曜日', 6, 'ゲームプログラミングc', '出席', '2021-10-26 14:15:02'),
+(3, 200001, 'ig21', '木曜日', 1, 'ゲームプログラミングc', '出席', '2021-10-28 09:31:51'),
+(4, 200002, 'ig21', '', 1, 'ゲームプログラミングc', '出席', '2021-11-02 05:18:28'),
+(5, 200005, 'ig21', '', 1, 'ゲームプログラミングc', '出席', '2021-11-02 05:19:05'),
+(6, 200004, 'ig21', '', 1, 'ゲームプログラミングc', '出席', '2021-11-02 05:19:05'),
+(7, 200003, 'ig21', '', 1, 'ゲームプログラミングc', '出席', '2021-11-02 05:19:49'),
+(8, 200004, 'ig21', '', 1, 'ゲームプログラミングc', '出席', '2021-11-02 05:19:49'),
+(9, 200004, 'ig21', '', 1, 'ゲームプログラミングc', '出席', '2021-11-02 05:22:59'),
+(10, 200003, 'ig21', '', 1, 'ゲームプログラミングc', '出席', '2021-11-02 05:22:59');
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `attendance_rate`
+--
+
+CREATE TABLE `attendance_rate` (
+  `id` int(11) NOT NULL,
+  `students_id` int(11) NOT NULL,
+  `timetable_subjects_id` varchar(40) NOT NULL,
+  `rate` varchar(4) NOT NULL,
   `update_time` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -82,16 +113,11 @@ CREATE TABLE `students` (
 --
 
 INSERT INTO `students` (`student_id`, `student_name`, `furigana`, `gender`, `email`, `password`, `classes_id`) VALUES
-(200001, '曽田航介', 'ソタコウスケ', '男', '200099oy@yse-c.net', '$2y$10$q7TOQ6tePjrKtlkSA5CidOyYChVnaiAsnReFYZbdBKFaTkVe5sVAO', 'ig21'),
-(200002, '中田久美子', 'ナカタクミコ', '女', 'a@a.aa', '$2y$10$h5KneXq8Xh4rI9jnxICWIO1loW9ka3LbdHnQHq6Cd/k.Mh/UCNVRu', 'ig22'),
-(200003, '田中太郎', 'タナカタロウ', '未設定', 'b@b.bb', '$2y$10$3aBeG8.xFCnBIggRX89JxuhTtYeutSkVnT9jnROXIPkmQZ8Q3ucEm', 'ri21'),
-(200004, '中田敦彦', 'ナカタアツヒコ', '男', 'c@c.cc', '$2y$10$eGUv5zpCPfSPKsBib3L1ZeXyOeyB21Dn3WG3HS8pkT8SYSldUw2mO', 'ri22'),
-(200005, '鈴木隆', 'スズキタカシ', '男', 'd@d.dd', '$2y$10$JMsZaLdrhZWBy2319TFGCu2zj0YxZDox7NgNbm8rUz4JJY1TS7zzq', 'ig11'),
-(200006, '佐藤一郎', 'サトウイチロウ', '未設定', 'e@e.ee', '$2y$10$FioIwLg/ZnWNmesosh6V8u9Rnd7YKX8bEj91uZQAp2ZxZXcwREYQG', 'ig12'),
-(200007, '伊藤健太', 'イトウケンタ', '未設定', 'f@f.ff', '$2y$10$VYJjDGcD1jWPDMyDq9SVd.vBFDXrD6YIB7WxY5hJUQ5vZag6c0LeO', 'ri11'),
-(200008, '奥田美和子', 'オクダミワコ', '女', 'g@g.gg', '$2y$10$IvVkxBP0lvKIYmE87rRiR.TtQlX0o.lo9h.2wJgoG1amVYeYnOBu2', 'ri12'),
-(200009, '新井純也', 'アライジュンヤ', '未設定', 'h@h.hh', '$2y$10$K5bhvLS/.75eq/qxdqh14eg6FapYUqrqk6IX.Goj2ai2LAdFO8ghC', 'ig21'),
-(200010, '武田勝', 'タケダマサル', '男', 'i@i.ii', '$2y$10$/t4rfLc06LXwv4fGVp3A2.z4bZjGJIf4.YJ8skLUdO8hfrkSgAbbq', 'ri11');
+(200001, '曽田航介', 'ソタコウスケ', '男', '200099oy@yse-c.net', '$2y$10$d9Mf0zTr5w86Mmg32CVN0OSbhVHGTngEoolQ6kjSzrvZarpzrVwIi', 'ig21'),
+(200002, 'あいうえお', 'アイウエオ', '男', 'a@a.aa', '$2y$10$jjVEug0VoCXE99PxoNpI8OQkp6/HqyGiR9d2i6g.jYqXiRtMSISym', 'ig21'),
+(200003, 'かきくけこ', 'カキクケコ', '未設定', 'c@c.cc', '$2y$10$Vk0r5OXIFAPjB8Pbgsp35u8vxe/QuTzsrSMgjN2jHlntd61E8T3.K', 'ig21'),
+(200004, 'さしすせそ', 'サシスセソ', '未設定', 'b@b.bb', '$2y$10$rBm9QnpN8RijOj/GUB3tOeAK3SgZUiiJJ5LDKj.5UzqZWkgiq6jfe', 'ig21'),
+(200005, 'たちつてと', 'タチツテト', '女', 'd@d.dd', '$2y$10$h9noRThEtzFtvEVxDWKYxeYkvRxRjGJCQ4Ls45CZujKnGTSmW7L5e', 'ig21');
 
 --
 -- ダンプしたテーブルのインデックス
@@ -103,7 +129,16 @@ INSERT INTO `students` (`student_id`, `student_name`, `furigana`, `gender`, `ema
 ALTER TABLE `attendance_log`
   ADD PRIMARY KEY (`id`),
   ADD KEY `students_id` (`students_id`) USING BTREE,
-  ADD KEY `timetable_subjects_id` (`timetable_subjects_id`);
+  ADD KEY `timetable_subjects_id` (`timetable_subjects_id`),
+  ADD KEY `attendance_log_ibfk_4` (`students_classes_id`);
+
+--
+-- テーブルのインデックス `attendance_rate`
+--
+ALTER TABLE `attendance_rate`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `timetable_subjects_id` (`timetable_subjects_id`),
+  ADD KEY `students_id` (`students_id`);
 
 --
 -- テーブルのインデックス `classes`
@@ -129,13 +164,19 @@ ALTER TABLE `students`
 -- テーブルの AUTO_INCREMENT `attendance_log`
 --
 ALTER TABLE `attendance_log`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- テーブルの AUTO_INCREMENT `attendance_rate`
+--
+ALTER TABLE `attendance_rate`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- テーブルの AUTO_INCREMENT `students`
 --
 ALTER TABLE `students`
-  MODIFY `student_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=200011;
+  MODIFY `student_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=200006;
 
 --
 -- ダンプしたテーブルの制約
@@ -147,7 +188,15 @@ ALTER TABLE `students`
 ALTER TABLE `attendance_log`
   ADD CONSTRAINT `attendance_log_ibfk_1` FOREIGN KEY (`students_id`) REFERENCES `students` (`student_id`) ON UPDATE CASCADE,
   ADD CONSTRAINT `attendance_log_ibfk_2` FOREIGN KEY (`timetable_subjects_id`) REFERENCES `timetable`.`subjects` (`subject_id`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `attendance_log_ibfk_3` FOREIGN KEY (`timetable_subjects_id`) REFERENCES `timetable`.`subjects` (`subject_id`) ON UPDATE CASCADE;
+  ADD CONSTRAINT `attendance_log_ibfk_3` FOREIGN KEY (`timetable_subjects_id`) REFERENCES `timetable`.`subjects` (`subject_id`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `attendance_log_ibfk_4` FOREIGN KEY (`students_classes_id`) REFERENCES `classes` (`class_id`) ON UPDATE CASCADE;
+
+--
+-- テーブルの制約 `attendance_rate`
+--
+ALTER TABLE `attendance_rate`
+  ADD CONSTRAINT `attendance_rate_ibfk_1` FOREIGN KEY (`timetable_subjects_id`) REFERENCES `timetable`.`subjects` (`subject_id`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `attendance_rate_ibfk_2` FOREIGN KEY (`students_id`) REFERENCES `students` (`student_id`) ON UPDATE CASCADE;
 
 --
 -- テーブルの制約 `students`
