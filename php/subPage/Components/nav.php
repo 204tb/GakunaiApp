0adb695d165dd841fc6895bddb2f35070121e082
204tb <200204tb@yse-c.net>
2021-11-30 13:10:06 +0900

    表示の修正

--- php/subPage/Components/nav.php
+++ php/subPage/Components/nav.php
@@ -16,7 +16,7 @@
     </button>
     <div class="collapse navbar-collapse" id="contents">
             <span class="row ref_992">
-                <a class="nav-link row ml-1" href="../出席管理/my_page.php" style="color:white;">マイページ</a>
+                <a class="nav-link row" href="../出席管理/my_page.php" style="color:white;">マイページ</a>
                 <a class="nav-link row ml-5" href="../時間割/Timeable.php" style="color:white";>時間割</a>
                 <a class="nav-link row ml-5" href="../掲示板/board.php" style="color:white";>掲示板</a>
                 <a href="../sign_out.php" class="nav-link float-right ml-4">サインアウト</a>
