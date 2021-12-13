d64e4dfe95e482925ade5a82450375d375d4e92f
204tb <200204tb@yse-c.net>
2021-12-13 09:42:15 +0900

    オフライン対応ページを追加

--- php/subPage/Components/nav.php
+++ php/subPage/Components/nav.php
@@ -1,12 +1,4 @@
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../../../css/sample.css">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
 
-    <title>Document</title>
-</head>
 <span class="defBarColor fixed-top" style="width: 100%;height:4em;;"><h1>　</h1>
 <nav class="container navbar navbar-expand-lg fixed-top navbar-light defBarColor">
     <h2><a class="" href="../index.php" style="color:white;">学内アプリ</a></h2>
--- php/subPage/時間割/Timeable.php
+++ php/subPage/時間割/Timeable.php
@@ -40,7 +40,7 @@ $timeTable = Get_TimeTable($pdo,$my_class);
 <html lang="en">
 <head>
     <link rel="stylesheet" href="../../../css/sample.css">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+    <link rel="stylesheet" href="../../../css/bootstrap.css">  
     <title>Document</title>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
